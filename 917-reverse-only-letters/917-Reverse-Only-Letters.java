a86f3358b906c708958a3c0265e112d6d87a2ba3
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-07-12 22:04:22 +0530

    [917] [Time Beats: 100.00%] [Memory Beats: 12.63%] - LeetPush

---
+++ 917-reverse-only-letters/917-Reverse-Only-Letters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char []ch = s.toCharArray();
+        int low = 0;
+        int high = s.length()-1;
+        while(low<high){
+            if(!Character.isLetter(ch[low])){
+                low++;
+            }else if(!Character.isLetter(ch[high])){
+                high--;
+            }
+            else{
+                char temp = ch[low];
+                ch[low] = ch[high];
+                ch[high] = temp;
+                low++;
+                high--;
+            }
+        }
+        return new String(ch);
+    }
+}
