3cd003e9065fa5cf276e1c00536fe32c000fb938
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-07-13 11:00:00 +0530

    [1137] [Time Beats: 100.00%] [Memory Beats: 53.06%] - LeetPush

---
+++ 1137-n-th-tribonacci-number/1137-N-th-Tribonacci-Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int tribonacci(int n) {
+        int[]F = new int[38];
+        F[0] = 0;
+        F[1] = F[2] = 1;
+        for(int i = 0; i<F.length-3;i++){
+            F[i+3] = F[i] + F[i+1] + F[i+2];
+        }
+        return F[n];
+
+
+    }
+}
