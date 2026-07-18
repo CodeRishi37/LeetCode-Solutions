caf0c869d14d968c19ed3f2bd428298597f583f9
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-07-18 12:43:34 +0530

    [1952] [Time Beats: 100.00%] [Memory Beats: 95.44%] - LeetPush

--- 1952-three-divisors/1952-Three-Divisors.java
+++ 1952-three-divisors/1952-Three-Divisors.java
@@ -1,4 +1,6 @@
+//Logic: Squares of all prime numbers have exactly 3 divisors: 1, its root, it
 class Solution {
+    //First we need a function to check if a number is prime or not
     public static boolean isPrime(int n){
         if(n<=1) return false;
         for(int i = 2; i*i<=n; i++){
@@ -7,9 +9,11 @@ class Solution {
         return true;
     }
     public boolean isThree(int n) {
-        if(n<=1) return false;
+        if(n<=1) return false;//For edge case
+        //Now we need to filter the perfect square ones
         int root = (int)Math.sqrt(n);
-        if(root*root!=n) return false;
-        return isPrime((int)Math.sqrt(n));
+        if(root*root!=n) return false; //Here we get the perfect squares e.g. 4,9,25...
+        //Now we are checking if its root is prime or not
+        return isPrime(root);
     }
 }
