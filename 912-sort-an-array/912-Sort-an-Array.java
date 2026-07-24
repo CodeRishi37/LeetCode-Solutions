65f9a327836f1a6342ac6becf705316f2d7a5a8f
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-07-24 12:21:26 +0530

    [912] [Time Beats: 5.00%] [Memory Beats: 5.07%] - LeetPush

--- 912-sort-an-array/912-Sort-an-Array.java
+++ 912-sort-an-array/912-Sort-an-Array.java
@@ -7,26 +7,26 @@ class Solution {
         nums[j] = temp;
     }
     public void quicksort(int[] nums, int start, int end){
-        if(start>=end) return;
+        if(start>=end) return; //We will end when start>end
         int randidx = start + random.nextInt(end-start+1);
         swap(nums,start,randidx);
-        int pivot = nums[start];
-        int i = start+1;
-        int j = end;
+        int pivot = nums[start];//First we will choose a random index starting from start yo end and swap the value with start, that will be out pivot
+        int i = start+1; //i will be next index of pivot
+        int j = end; //j will be last index
         while(i<=j){
-            while(i<=end&&nums[i]<=pivot){
+            while(i<=end&&nums[i]<=pivot){ //we will stop i index while traversing from i to end at where nums[i]>pivot
                 i++;
             }
-            while(j>start&&nums[j]>=pivot){
+            while(j>start&&nums[j]>=pivot){ //we will stop j index while traversing from j to start at where nums[j]<=pivot
                 j--;
             }
-            if(i<j){
+            if(i<j){ //if i<j or i and j not crossed eachother, we will swap them
                 swap(nums,i,j);
             }
         }
-        swap(nums,start,j);
-        quicksort(nums,start,j-1);
-        quicksort(nums,j+1,end);
+        swap(nums,start,j); //here i and j crossed eachother, so we will swap pivot element with nums[j]
+        quicksort(nums,start,j-1);//for the left side of pivot
+        quicksort(nums,j+1,end);//for the right side of pivot
     }
     public int[] sortArray(int[] nums) {
         quicksort(nums,0,nums.length-1);
