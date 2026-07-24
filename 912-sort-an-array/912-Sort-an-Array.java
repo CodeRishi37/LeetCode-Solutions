002e876d65d83711b61c6b031973eceb50122eac
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-07-24 20:59:17 +0530

    [912] [Time Beats: 48.94%] [Memory Beats: 53.09%] - LeetPush

--- 912-sort-an-array/912-Sort-an-Array.java
+++ 912-sort-an-array/912-Sort-an-Array.java
@@ -1,35 +1,50 @@
-import java.util.Random;
+//Merge Sort TC: O(nlogn) SC: O(n)
 class Solution {
-    Random random = new Random();
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    public void quicksort(int[] nums, int start, int end){
-        if(start>=end) return;
-        int randidx = start + random.nextInt(end-start+1);
-        swap(nums,start,randidx);
-        int pivot = nums[start];
-        int i = start+1;
-        int j = end;
-        while(i<=j){
-            while(i<=end&&nums[i]<=pivot){
-                i++;
-            }
-            while(j>start&&nums[j]>=pivot){
-                j--;
-            }
-            if(i<j){
-                swap(nums,i,j);
-            }
+    public void mergeSort(int[] nums, int start, int end){
+        if(start<end){
+            int mid = start + (end-start)/2;
+            mergeSort(nums,start,mid);
+            mergeSort(nums,mid+1,end);
+            merge(nums,start,mid,end);
         }
-        swap(nums,start,j);
-        quicksort(nums,start,j-1);
-        quicksort(nums,j+1,end);
+    }
+    public void merge(int[] nums, int start, int mid, int end){
+        int n1 = mid-start+1;
+        int n2 = end-mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for(int i = 0; i<n1; i++){
+            left[i] = nums[start+i];
+        }
+        for(int j = 0; j<n2; j++){
+            right[j] = nums[mid+1+j];
+        }
+        int i = 0;
+        int j = 0;
+        int k = start;
+        while(i<n1&&j<n2){
+            if(left[i]<=right[j]){
+                nums[k] = left[i];
+                i++;
+            }else{
+                nums[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1){
+            nums[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            nums[k] = right[j];
+            j++;
+            k++;
+        }
     }
     public int[] sortArray(int[] nums) {
-        quicksort(nums,0,nums.length-1);
+        mergeSort(nums,0,nums.length-1);
         return nums;
     }
 }
