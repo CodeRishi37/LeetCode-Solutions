3fb7cf7bf3155fe3fccdaaa47ee24c1866b449c6
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-08-04 21:37:29 +0530

    [2540] [Time Beats: 100.00%] [Memory Beats: 50.03%] - LeetPush

---
+++ 2540-minimum-common-value/2540-Minimum-Common-Value.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        if(nums1[0]==nums2[0]) return nums1[0];
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1==0||n2==0) return -1;
+        if(nums1[0]>nums2[n2-1]||nums2[0]>nums1[n1-1]) return -1;
+        int i = 0;
+        int j = 0;
+        
+        while(i<n1&&j<n2){
+            if(nums1[i]==nums2[j]) return nums1[i];
+            if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return -1;
+    }
+}
