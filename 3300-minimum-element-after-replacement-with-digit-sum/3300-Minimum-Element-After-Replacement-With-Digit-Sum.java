0e137d211733a1d2784a6fcd733bc6012288d765
Saptarshi Patra <85726419+CodeRishi37@users.noreply.github.com>
2026-08-05 00:13:45 +0530

    [3300] [Time Beats: 100.00%] [Memory Beats: 58.76%] - LeetPush

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-Minimum-Element-After-Replacement-With-Digit-Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int digitSum(int num){
+        int sum = 0;
+        while(num>0){
+            sum += num%10;
+            num/=10;
+        }
+        return sum;
+    }
+    public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<nums.length; i++){
+            nums[i] = digitSum(nums[i]);
+            min = Math.min(min,nums[i]);
+        }
+        return min;
+    }
+}
